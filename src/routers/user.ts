1f82fe00b9c7a17efcaa1b61f4ed1c9e8a364e73
JanithMS <janithms9920@gmail.com>
2021-03-31 00:17:40 +0530

    edit profile modified

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -13,7 +13,6 @@ router.post('/api/signup', validate, async (req, res) => {
     try {
         const user = await client.query("select * from users where user_email = $1",[email]);
         
-        //console.log(user.rows.length != 0)
         if (user.rows.length > 0 ){
             return res.status(401).send('Email has registered already');
         }
@@ -27,7 +26,6 @@ router.post('/api/signup', validate, async (req, res) => {
 
         return res.status(201).send({name, userjwtToken});
     } catch (e) {
-        //console.log('Error: ' + e)
         return res.status(500).send(e.detail)
     }
 });
@@ -42,7 +40,6 @@ router.post('/api/signin', validate, async (req, res) => {
             return res.status(401).send('Invalid Credentials')
         }
 
-        //return console.log(password, user.rows[0].user_password);
         const password_check = await bcrypt.compare(password, user.rows[0].user_password)
         if (!password_check){
             return res.status(401).send('Invalid Credentials');
@@ -56,36 +53,31 @@ router.post('/api/signin', validate, async (req, res) => {
     }
 });
 
-router.patch('/api/editaccount', validate, async (req, res) => {
-    const {jwttoken, name, password} = req.body;
+router.patch('/api/editprofile', validate, async (req, res) => {
 
-    //const a = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ5NzUwMWRjLWZjMjMtNDYxMC05Mzk5LTU2NGE3M2UzMGEyOSIsImlhdCI6MTYxNjgxNTkyNX0.8pa69Xulualo2x2Z9MM8gWEzJG8dl4gQeZK4t3545Bc";
+    const updatekeys = Object.keys(req.body);
+    const allowedkeyupdates = ['user_name', 'user_password'];
+    const isupdates = updatekeys.every((updatekey) => allowedkeyupdates.includes(updatekey));
+
+    const jwttoken = req.headers['token']?.toString();
+    if(!jwttoken) {
+        return res.status(401).send("Please Login")
+    }
+    const userid = jwtDecode(jwttoken)
+
+    if (!isupdates) {
+        return res.status(400).send('Invalid updates!')
+    }
+
     try {
-        if(!jwttoken){
-            return res.status(401).send("Bad Request")
-        }
-
-        if(name && password){
-            await client.query("update users set user_name=$1 where user_id = $2", [name, jwtDecode(jwttoken).id]);
-            const bcryptPassword = await bcrypt.hash(password, 10);
-            await client.query("update users set user_password=$1 where user_id=$2", [bcryptPassword, jwtDecode(jwttoken).id]);
-            return res.status(201).send("Profile name and Password Updated")
-        }
-        else if(name){
-            await client.query("update users set user_name=$1 where user_id = $2", [name, jwtDecode(jwttoken).id]);
-            return res.status(201).send("Profile name updated")
-        } else if (password) {
-            const bcryptPassword = await bcrypt.hash(password, 10);
-            await client.query("update users set user_password=$1 where user_id=$2", [bcryptPassword, jwtDecode(jwttoken).id]);
-            return res.status(201).send("Password Updated")
-        }
-
-        return res.status(401).send("Bad Request")
+        updatekeys.forEach(async (updatekey) => {
+            const str = 'update users set ' + updatekey + ' = $1 where user_id = $2';
+            await client.query(str,[req.body[updatekey], userid]);
+        });
+        return res.send('Profile Updated')
     } catch (e) {
-        console.log(e)
         return res.status(500).send(e.detail)
     }
-    //res.send(user.rows[0])
 })
 
 export default router
