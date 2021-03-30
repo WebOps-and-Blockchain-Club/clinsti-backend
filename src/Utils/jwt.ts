38e8378cb6e9afd2c94d3430f691386e2c54cc11
JanithMS <janithms9920@gmail.com>
2021-03-30 21:41:55 +0530

    jwt verify added

--- src/Utils/jwt.ts
+++ src/Utils/jwt.ts
@@ -2,12 +2,13 @@ const jwt = require("jsonwebtoken");
 import * as dotenv from "dotenv";
 dotenv.config();
 
-function jwtToken(user_id: any) {
-    return jwt.sign({id: user_id}, process.env.jwtSecret)
+function jwtToken(user_id: String) {
+    return jwt.sign({_id: user_id}, process.env.jwtSecret, { expiresIn: '30days'})
 }
 
-function jwtDecode(token: any) {
-    return jwt.decode(token)
+function jwtDecode(token: String) {
+    var decoded = jwt.verify(token, process.env.jwtSecret)
+    return decoded._id
 }
 
 export {jwtToken, jwtDecode};
