a10b02b5377852a30edc93632a594c7f1a026fcb
JanithMS <janithms9920@gmail.com>
2021-04-01 00:00:48 +0530

    middleware added to get userID

---
+++ src/middleware/userID.ts
@@ -0,0 +1,19 @@
+import {jwtDecode} from '../Utils/jwt';
+
+const userID = async (req: any, res: any, next: any) => {
+    const jwttoken = req.headers['authorization']?.replace('Bearer ', '')
+
+    if(!jwttoken) {
+        return res.status(401).send("Please Login")
+    }
+
+    const {id:userId, error} = await jwtDecode(jwttoken)
+    if(error) {
+        return res.status(401).send(error)
+    }
+
+    req.body.userID = userId;
+    next();
+}
+
+export default userID;
