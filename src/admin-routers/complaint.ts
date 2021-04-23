a26a9a5068e34ecad084bf6cd2030f7068c9f8f4
JanithMS <janithms9920@gmail.com>
2021-04-23 17:58:06 +0000

    Revert "added option in admin complaint list to get complaint in date range"
    This reverts commit a5b55ec9b14f089bf3e99f6e111547bef8a9b307

--- src/admin-routers/complaint.ts
+++ src/admin-routers/complaint.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import client from '../../postgres';
-//import adminAuth from '../middleware/admin-auth';
+import adminAuth from '../middleware/admin-auth';
 
 const router = express.Router();
 
-router.get('/admin/complaints',/* adminAuth,*/ async (req, res) => {
+router.get('/admin/complaints', adminAuth, async (req, res) => {
 
-    const {status, zone, dateFrom, dateTo} = req.body;
+    const {status, zone} = req.body;
 
     try{
         const result = await client.query(
-            `select complaint_id, created_time, status from complaints 
+            `select _location, created_time, status from complaints 
             where status IN ${status} and 
-            zone IN ${zone} and
-            created_time >= '${dateFrom}' and
-            created_time <= '${dateTo}'`
+            zone IN ${zone}`
         );
         
         if(result.rowCount === 0){
